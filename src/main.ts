c9970b0cf8bab0e943891c71817a3fb46811f436
grishO92 <grigorgiutta@gmail.com>
2023-07-07 23:58:20 +0200

    added register

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+import { provideStore } from '@ngrx/store';
 
-bootstrapApplication(AppComponent, { providers: [] });
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes), provideStore()],
+});
