05d08b60dbcb8600a8788208c0788f92f24a9a5b
grishO92 <grigorgiutta@gmail.com>
2023-07-08 15:53:03 +0200

    updated actions

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,7 +1,13 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 import { RegisterRequestInterface } from '../types/registerRequest.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { BackEndErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
 
-export const register = createAction(
-  '[Auth] Register',
-  props<{ request: RegisterRequestInterface }>()
-);
+export const authActions = createActionGroup({
+  source: 'auth',
+  events: {
+    Register: props<{ request: RegisterRequestInterface }>(),
+    'Register Success': props<{ currentUser: CurrentUserInterface }>(),
+    'Register Failure': props<{ errors: BackEndErrorsInterface }>(),
+  },
+});
