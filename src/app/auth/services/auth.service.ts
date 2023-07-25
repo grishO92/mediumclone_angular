51a5c8d2a695291fa3c7a8d57e69fd6eb377d47a
grishO92 <grigorgiutta@gmail.com>
2023-07-25 11:23:07 +0200

    added login and getUser methods

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,15 +5,27 @@ import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface
 import { Observable, map } from 'rxjs';
 import { AuthResponseInterface } from '../types/authResponse.interface';
 import { environment } from 'src/environments/environment.development';
+import { LoginRequestInterface } from '../types/lloginRequest.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user;
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users';
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((response) => response.user));
+      .pipe(map(this.getUser));
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login';
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser));
   }
 }
