efc64e45a7479c9c11a616836bdcfac043323e71
grishO92 <grigorgiutta@gmail.com>
2023-07-25 11:23:57 +0200

    added login actions

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -2,6 +2,7 @@ import { createActionGroup, props } from '@ngrx/store';
 import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 import { BackEndErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
+import { LoginRequestInterface } from '../types/lloginRequest.interface';
 
 export const authActions = createActionGroup({
   source: 'auth',
@@ -9,5 +10,9 @@ export const authActions = createActionGroup({
     Register: props<{ request: RegisterRequestInterface }>(),
     'Register Success': props<{ currentUser: CurrentUserInterface }>(),
     'Register Failure': props<{ errors: BackEndErrorsInterface }>(),
+
+    Login: props<{ request: LoginRequestInterface }>(),
+    'Login Success': props<{ currentUser: CurrentUserInterface }>(),
+    'Login Failure': props<{ errors: BackEndErrorsInterface }>(),
   },
 });
