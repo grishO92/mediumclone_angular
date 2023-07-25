702e7209b513a0673ec57d7c8eed8ae14965454a
grishO92 <grigorgiutta@gmail.com>
2023-07-25 12:01:55 +0200

    added login component + logic

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { authActions } from '../../store/actions';
+import { LoginRequestInterface } from '../../types/loginRequest.interface';
+import { RouterLink } from '@angular/router';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+
+import { CommonModule } from '@angular/common';
+import { combineLatest } from 'rxjs';
+import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component';
+
+@Component({
+  selector: 'mc-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    BackendErrorMessages,
+  ],
+})
+export class loginComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+    const request: LoginRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(authActions.login({ request }));
+  }
+}
