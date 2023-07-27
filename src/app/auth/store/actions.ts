f39687c31780ec56f73e12ff55eb20e53b2eb744
grishO92 <grigorgiutta@gmail.com>
2023-07-27 22:54:58 +0200

    added curremtUser actions

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,4 +1,4 @@
-import { createActionGroup, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 import { BackEndErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
@@ -14,5 +14,9 @@ export const authActions = createActionGroup({
     Login: props<{ request: LoginRequestInterface }>(),
     'Login Success': props<{ currentUser: CurrentUserInterface }>(),
     'Login Failure': props<{ errors: BackEndErrorsInterface }>(),
+
+    'Get current user': emptyProps(),
+    'Get current user Success': props<{ currentUser: CurrentUserInterface }>(),
+    'Get current user Failure': emptyProps(),
   },
 });
