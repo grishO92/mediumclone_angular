6f6cf0d1fc714dce7398ec72834e758fabe4b79a
grishO92 <grigorgiutta@gmail.com>
2023-07-30 12:21:42 +0200

    created barebone globalFeed component

---
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-globalFeed',
+  templateUrl: './globalFeed.component.html',
+  standalone: true,
+})
+export class GlobalFeedComponent {}
