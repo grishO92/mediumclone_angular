573d01b4ce6f4ea51d403fc34ced7154841579d7
grishO92 <grigorgiutta@gmail.com>
2023-07-30 12:31:42 +0200

    imported feed component to globalFeed component

--- src/app/globalFeed/components/globalFeed/globalFeed.component.ts
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FeedComponent } from 'src/app/shared/components/feed/feed.component';
 
 @Component({
   selector: 'mc-globalFeed',
   templateUrl: './globalFeed.component.html',
   standalone: true,
+  imports: [FeedComponent],
 })
 export class GlobalFeedComponent {}
