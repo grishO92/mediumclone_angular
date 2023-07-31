566941963a4aa12c4d792e9cd05fb62c8047db18
grishO92 <grigorgiutta@gmail.com>
2023-07-31 13:13:38 +0200

    registered feed effects and reducers

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,14 @@ import { isDevMode } from '@angular/core';
 import { authFeatureKey, authReducer } from './app/auth/store/reducers';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import * as authEffects from './app/auth/store/effects';
+import * as feedEffects from './app/shared/components/feed/store/effects';
 import { provideEffects } from '@ngrx/effects';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { authInterceptor } from './app/shared/services/authInterceptor';
+import {
+  feedFeatureKey,
+  feedReducer,
+} from './app/shared/components/feed/store/reducers';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -19,7 +24,8 @@ bootstrapApplication(AppComponent, {
     provideStore({ router: routerReducer }),
     provideRouterStore(),
     provideState(authFeatureKey, authReducer),
-    provideEffects(authEffects),
+    provideState(feedFeatureKey, feedReducer),
+    provideEffects(authEffects, feedEffects),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
