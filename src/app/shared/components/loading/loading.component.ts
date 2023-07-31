44174202089a1d3e09dbf7a090e1f231e12714c7
grishO92 <grigorgiutta@gmail.com>
2023-07-31 13:48:33 +0200

    created a basic loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-loading',
+  templateUrl: './loading.component.html',
+  standalone: true,
+})
+export class LoadingComponent {}
