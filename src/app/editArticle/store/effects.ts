edc83e330ae4c0555becdb7e33f12be9dc4b3ec9
grishO92 <grigorgiutta@gmail.com>
2023-11-02 22:04:04 +0100

    added store to edit article

---
+++ src/app/editArticle/store/effects.ts
@@ -0,0 +1,67 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { switchMap, map, catchError, of, tap } from 'rxjs';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+import { editArticleActions } from './actions';
+import { EditArticleService } from '../services/editArticle.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ArticleService as SharedArticleService } from 'src/app/shared/services/article.service';
+
+export const getArticleEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+    articleService = inject(SharedArticleService)
+  ) => {
+    return actions$.pipe(
+      ofType(editArticleActions.getArticle),
+      switchMap(({ slug }) => {
+        return articleService.getArticle(slug).pipe(
+          map((article: ArticleInterface) => {
+            return editArticleActions.getArticleSuccess({ article });
+          }),
+          catchError(() => {
+            return of(editArticleActions.getArticleFailure());
+          })
+        );
+      })
+    );
+  },
+  { functional: true }
+);
+
+export const updateArticleEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+    editArticleService = inject(EditArticleService)
+  ) => {
+    return actions$.pipe(
+      ofType(editArticleActions.updateArticle),
+      switchMap(({ request, slug }) => {
+        return editArticleService.updateArticle(slug, request).pipe(
+          map((article: ArticleInterface, slug) => {
+            return editArticleActions.updateArticleSuccess({ article });
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              editArticleActions.updateArticleFailure({
+                errors: errorResponse.error.errors,
+              })
+            );
+          })
+        );
+      })
+    );
+  },
+  { functional: true }
+);
+
+export const redirectAfterUpdateEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(editArticleActions.updateArticleSuccess),
+      tap(({ article }) => router.navigate(['/articles', article.slug]))
+    );
+  },
+  { functional: true, dispatch: false }
+);
