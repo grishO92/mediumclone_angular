a1260c1639a5bf7f8b1f563d521f02a32ee0ba9b
grishO92 <grigorgiutta@gmail.com>
2023-12-03 20:10:31 +0100

    added store/reducers for setting feature

---
+++ src/app/settings/store/reducers.ts
@@ -0,0 +1,34 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { SettingsTypeInterface } from '../types/settingsType.interface';
+import { authActions } from 'src/app/auth/store/actions';
+import { routerNavigatedAction } from '@ngrx/router-store';
+
+const initialState: SettingsTypeInterface = {
+  isSubmitting: false,
+  validationErrors: null,
+};
+
+const settingsFeature = createFeature({
+  name: 'settings',
+  reducer: createReducer(
+    initialState,
+    on(authActions.updateCurrentUser, (state) => ({
+      ...state,
+    })),
+    on(authActions.updateCurrentUserSuccess, (state) => ({
+      ...state,
+    })),
+    on(authActions.updateCurrentUserFailure, (state, action) => ({
+      ...state,
+      validationErrors: action.errors,
+    })),
+    on(routerNavigatedAction, () => initialState)
+  ),
+});
+
+export const {
+  name: settingsFeatureKey,
+  reducer: settingsReducer,
+  selectValidationErrors,
+  selectIsSubmitting,
+} = settingsFeature;
