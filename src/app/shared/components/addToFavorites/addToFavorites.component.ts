6e60de746f481fbfb2be81dc9c483212e5308b34
grishO92 <grigorgiutta@gmail.com>
2024-01-09 14:18:15 +0100

    added logic for getting the state from the api (real updates)

--- src/app/shared/components/addToFavorites/addToFavorites.component.ts
+++ src/app/shared/components/addToFavorites/addToFavorites.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addToFavoritesActions } from './store/actions';
 
 @Component({
   selector: 'mc-add-to-favorites',
@@ -8,11 +10,20 @@ import { Component, Input } from '@angular/core';
   imports: [CommonModule],
 })
 export class AddToFavoritesComponent {
+  store = inject(Store);
+
   @Input() isFavorited: boolean = false;
   @Input() favoritesCount: number = 0;
   @Input() articleSlug: string = '';
 
   handleLike(): void {
+    this.store.dispatch(
+      addToFavoritesActions.addToFavorites({
+        isFavorited: this.isFavorited,
+        slug: this.articleSlug,
+      })
+    );
+
     this.isFavorited ? this.favoritesCount-- : this.favoritesCount++;
 
     this.isFavorited = !this.isFavorited;
